d3256f7880c504b16bc4d3e8d451910f0bff6760
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-04-10 17:09:40 +0300

    initialized variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const competitionList = document.getElementById("competition-list");
+const searchInput = document.getElementById("search-input");
+const filterButtons = document.querySelectorAll(".filters button");
+const searchFilterButton = document.getElementById("search-filter-btn");
